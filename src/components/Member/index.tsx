f644fc35c5f331c9c73295893aceb07c3397348f
ozline <ozlinex@outlook.com>
2024-06-05 15:10:13 +0800

    feat: complete member list component

--- src/components/Member/index.tsx
+++ src/components/Member/index.tsx
@@ -1,58 +1,95 @@
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
+import { Avatar } from "@/components/ui/avatar"
 import { memberData } from "@/components/Member/memberData";
-import React, { useState, useEffect } from 'react';
-
-async function loadImage(avatar) {
-  try {
-    const image = await import(`@site/static/member/${avatar}`);
-    return image.default;
-  } catch (e) {
-    console.error('Failed to load image', e);
-    return '@site/static/img/logo.svg'; // 返回一个占位图的路径或者你想要的错误图像
-  }
-}
+import React, { useState, useMemo, useEffect } from 'react';
+import LazyLoad from '@parrotjs/react-lazyload'
 
 export default function Component() {
   const data = memberData
-  const [images, setImages] = useState({});
+  const [activeYear, setActiveYear] = useState(Object.keys(data)[0]); // 默认为第一个年份
+  const [activeFocus, setActiveFocus] = useState('All'); // 默认选中所有 focus
 
+  // 当 activeYear 改变时，检查 focus 是否存在于新的年份中
   useEffect(() => {
-    (async () => {
-      const promises = Object.values(data).flat().map(async member => {
-        const src = await loadImage(member.avatar);
-        return { [member.avatar]: src };
-      });
+    const focuses = new Set(data[activeYear].map(member => member.focus));
+    if (!focuses.has(activeFocus) && activeFocus !== 'All') {
+      setActiveFocus('All');
+    }
+  }, [activeYear, activeFocus, data]);
 
-      const imagesArray = await Promise.all(promises);
-      const imagesObject = imagesArray.reduce((acc, current) => ({ ...acc, ...current }), {});
-      setImages(imagesObject);
-    })();
-  }, []); // 空依赖数组意味着这个 effect 只在组件挂载时运行
+  // 提取当前年份所有成员的 focus 及对应人数
+  const focusOptionsWithCount = useMemo(() => {
+    const focusCountMap = data[activeYear].reduce((acc, member) => {
+      acc[member.focus] = (acc[member.focus] || 0) + 1;
+      return acc;
+    }, {});
+    const focusesWithCount = Array.from(new Set(data[activeYear].map(member => member.focus))).map(focus => ({
+      focus,
+      count: focusCountMap[focus]
+    }));
+    return [{ focus: 'All', count: data[activeYear].length }, ...focusesWithCount];
+  }, [activeYear, data]);
+
+  // 过滤成员数据基于当前选定的 focus
+  const filteredMembers = useMemo(() => {
+    if (activeFocus === 'All') {
+      return data[activeYear];
+    }
+    return data[activeYear].filter(member => member.focus === activeFocus);
+  }, [activeYear, activeFocus, data]);
 
   return (
     <div className="container mx-auto py-12 px-4 md:px-6">
-      {Object.keys(data).map((year) => (
-        <div key={year} className="mb-12">
-          <h2 className="text-2xl font-bold mb-6">{year}</h2>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-            {data[year].map((member, index) => (
-              <div
-                key={index}
-                className="flex flex-col items-center gap-2 bg-white dark:bg-gray-950 p-4 rounded-lg shadow-md"
-              >
-                <Avatar>
-                <img src={images[member.avatar]} alt={member.name} />
-                  <AvatarFallback>{member.name.split(" ").map((word) => word[0])}</AvatarFallback>
-                </Avatar>
-                <div className="text-center">
-                  <h3 className="font-semibold">{member.name}</h3>
-                  <p className="text-gray-500 dark:text-gray-400 text-sm">{member.profession}</p>
-                </div>
-              </div>
-            ))}
+      {/* 年份选择 */}
+      <div className="mb-4 flex space-x-2">
+        {Object.keys(data).map((year) => (
+          <div
+              key={year}
+              className={`px-4 py-2 mr-2 mb-2 whitespace-nowrap rounded text-sm font-medium transition ease-in-out duration-300 cursor-pointer
+                        ${activeYear === year ? 'bg-[var(--ifm-color-primary)] text-white' : 'text-[var(--ifm-color-primary)] bg-gray-200 hover:bg-[var(--ifm-color-primary-lightest)] hover:text-[var(--ifm-color-primary-dark)]'}`}
+              onClick={() => setActiveYear(year)}
+            >
+              {year} ({data[year].length})
           </div>
+
+
+        ))}
+      </div>
+      {/* Focus 选择 */}
+      <div className="mb-12">
+        <h2 className="text-2xl font-bold mb-6">{activeYear + "年秋季"}</h2>
+        <div className="mb-4 flex flex-wrap">
+          {focusOptionsWithCount.map(({ focus, count }) => (
+            <div
+              key={focus}
+              className={`px-4 py-2 mr-2 mb-2 whitespace-nowrap rounded text-sm font-medium transition ease-in-out duration-300 cursor-pointer
+                        ${activeFocus === focus ? 'bg-[var(--ifm-color-primary)] text-white' : 'text-[var(--ifm-color-primary)] bg-gray-200 hover:bg-[var(--ifm-color-primary-lightest)] hover:text-[var(--ifm-color-primary-dark)]'}`}
+              onClick={() => setActiveFocus(focus)}
+            >
+              {focus} ({count})
+            </div>
+          ))}
         </div>
-      ))}
+        {/* 成员展示 */}
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+          {filteredMembers.map((member) => (
+            <div
+              key={member.avatar}
+              className="flex flex-col items-center gap-2 bg-white dark:bg-gray-950 p-4 rounded-lg shadow-md"
+            >
+              <Avatar>
+                <LazyLoad>
+                  <img src={"https://img.w2fzu.com/member/" + member.avatar} alt={member.name} className="w-20 h-20 rounded-full" />
+                </LazyLoad>
+              </Avatar>
+              <div className="text-center">
+                <h3 className="font-semibold">{member.name}</h3>
+                <p className="text-gray-500 dark:text-gray-400 text-sm">{member.major}</p>
+                {member.github !== "null" && <a href={"https://github.com/" + member.github}>{member.github}</a>}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Member/memberData.ts
+++ src/components/Member/memberData.ts
@@ -1,8 +1,9 @@
 type MemberData = Record<string, {
-  avatar: string;
-  name: string;
-  profession: string;
-  github: string;
+  avatar: string;     // 头像
+  name: string;       // 姓名
+  focus: string;      // 专注于
+  github: string;     // GitHub 地址
+  major: string;      // 专业
 }[]>
 
 export const memberData: MemberData = {
@@ -13,15 +14,57 @@ export const memberData: MemberData = {
     // { avatar: "/placeholder-avatar.jpg", name: "Emily Davis", profession: "Data Analyst" },
   ],
   "2022": [
-    { avatar: "2022/android_sqt.jpg", name: "David Lee", profession: "Backend Developer", github: "string" },
-    { avatar: "/placeholder-avatar.jpg", name: "Sarah Kim", profession: "Frontend Developer", github: "string" },
-    { avatar: "/placeholder-avatar.jpg", name: "Tom Wilson", profession: "Project Manager", github: "string" },
-    { avatar: "/placeholder-avatar.jpg", name: "Olivia Chen", profession: "QA Engineer", github: "string" },
-  ],
-  "2021": [
-    // { avatar: "/placeholder-avatar.jpg", name: "Alex Park", profession: "DevOps Engineer" },
-    // { avatar: "/placeholder-avatar.jpg", name: "Sophia Nguyen", profession: "Business Analyst" },
-    // { avatar: "/placeholder-avatar.jpg", name: "Ryan Gonzalez", profession: "Mobile Developer" },
-    // { avatar: "/placeholder-avatar.jpg", name: "Isabella Ramirez", profession: "Content Writer" },
+    { avatar: "2022/frontends_python_zyy.jpg", name: "张艺耀", focus: "前端", major:"21级人工智能", github: "null" },
+    { avatar: "2022/frontends_python_wy.jpg", name: "吴悠", focus: "Python", major:"21级给排水", github: "null" },
+
+    { avatar: "2022/unity_lqy.jpg", name: "罗骐翼", focus: "Unity", major:"21级数媒", github: "null" },
+    { avatar: "2022/unity_hzy.jpg", name: "黄致远", focus: "Unity", major:"21级计算机", github: "null" },
+    { avatar: "2022/unity_wjy.jpg", name: "吴佳仪", focus: "Unity", major:"20级自动化", github: "null" },
+    { avatar: "2022/unity_hhc.jpg", name: "何晗晨", focus: "Unity", major:"21级数媒", github: "null" },
+    { avatar: "2022/unity_yxx.jpg", name: "袁夏鑫", focus: "Unity", major:"21级软件工程", github: "null" },
+    { avatar: "2022/unity_hkh.jpg", name: "黄铠赫", focus: "Unity", major:"21级数媒", github: "null" },
+    { avatar: "2022/unity_ywx.jpg", name: "姚玮欣", focus: "Unity", major:"20级计算机", github: "null" },
+
+    { avatar: "2022/golang_ckf.jpg", name: "陈恺丰", focus: "Go", major:"20级电子信息工程", github: "null" },
+    { avatar: "2022/golang_fyc.jpg", name: "方宇辰", focus: "Go", major:"20级软件工程", github: "null" },
+    { avatar: "2022/golang_zzy.jpg", name: "张钊洋", focus: "Go", major:"20级电子信息工程", github: "null" },
+    { avatar: "2022/frontends_ios_golang_lhx.jpg", name: "林黄骁", focus: "Go", major:"21级软件工程", github: "null" },
+
+    { avatar: "2022/python_cxy.jpg", name: "陈昕远", focus: "Python", major:"20级软件工程", github: "null" },
+    { avatar: "2022/python_gbq.jpg", name: "高炳强", focus: "Python", major:"21级人工智能", github: "null" },
+    { avatar: "2022/python_lzp.jpg", name: "李志鹏", focus: "Python", major:"20级计算机", github: "null" },
+
+    { avatar: "2022/android_sqt.jpg", name: "沈轻腾", focus: "Android", major:"21级计算机", github: "null" },
+    { avatar: "2022/android_zyn.jpg", name: "张煜楠", focus: "Android", major:"21级软件工程", github: "null" },
+    { avatar: "2022/android_sx.jpg", name: "孙逊", focus: "Android", major:"21级电子信息工程", github: "null" },
+
+    { avatar: "2022/frontends_zjz.jpg", name: "张佳泽", focus: "前端", major:"20级软件工程", github: "null" },
+    { avatar: "2022/frontends_czh.jpg", name: "陈泽华", focus: "前端", major:"20级软件工程", github: "null" },
+    { avatar: "2022/frontends_pcy.jpg", name: "庞财莹", focus: "前端", major:"21级软件工程", github: "null" },
+    { avatar: "2022/frontends_wxy.jpg", name: "吴星宇", focus: "前端", major:"21级计算机", github: "null" },
+    { avatar: "2022/frontends_gcj.jpg", name: "龚辰珺", focus: "前端", major:"20级软件工程", github: "null" },
+    { avatar: "2022/frontends_lx.jpg", name: "林翔", focus: "前端", major:"20级软件工程", github: "null" },
+    { avatar: "2022/frontends_wzr.jpg", name: "吴子隆", focus: "前端", major:"20级软件工程", github: "null" },
+
+    { avatar: "2022/java_lb.jpg", name: "刘博", focus: "Java", major:"20级自动化", github: "null" },
+    { avatar: "2022/java_czh.jpg", name: "陈桢函", focus: "Java", major:"20级软件工程", github: "null" },
+    { avatar: "2022/java_zl.jpg", name: "朱辽", focus: "Java", major:"20级自动化", github: "null" },
+    { avatar: "2022/java_hjq.jpg", name: "胡家齐", focus: "Java", major:"20级自动化", github: "null" },
+    { avatar: "2022/java_lyd.jpg", name: "李炎东", focus: "Java", major:"21级软件工程", github: "null" },
+    { avatar: "2022/java_ljh.jpg", name: "刘俊辉", focus: "Java", major:"21级人工智能", github: "null" },
+    { avatar: "2022/java_lzp.jpg", name: "李振鹏", focus: "Java", major:"20级软件工程", github: "null" },
+    { avatar: "2022/java_xyj.jpg", name: "许影杰", focus: "Java", major:"20级软件工程", github: "null" },
+    { avatar: "2022/java_cy.jpg", name: "陈艺", focus: "Java", major:"21级计算机", github: "null" },
+    { avatar: "2022/java_sjs.jpg", name: "苏锦森", focus: "Java", major:"21级计算机", github: "null" },
+    { avatar: "2022/java_yrw.jpg", name: "杨润午", focus: "Java", major:"20级软件工程", github: "null" },
+    { avatar: "2022/java_yjj.jpg", name: "余婧洁", focus: "Java", major:"20级自动化", github: "null" },
+    { avatar: "2022/java_zcx.jpg", name: "张晨旭", focus: "Java", major:"20级计算机", github: "null" },
+    { avatar: "2022/java_czf.jpg", name: "陈知菲", focus: "Java", major:"21级计算机", github: "null" },
+
+    { avatar: "2022/art_lcy.jpg", name: "李宸晔", focus: "美术", major:"20级建筑学", github: "null" },
+    { avatar: "2022/art_lmq.jpg", name: "李鸣谦", focus: "美术", major:"20级信息安全", github: "null" },
+    { avatar: "2022/art_cjw.jpg", name: "陈佳雯", focus: "美术", major:"21级数媒", github: "null" },
+    { avatar: "2022/art_lxy.jpg", name: "林心悦", focus: "美术", major:"21级数媒", github: "null" },
+    { avatar: "2022/art_hcx.jpg", name: "黄晨曦", focus: "美术", major:"21级信管", github: "null" },
   ],
 }
