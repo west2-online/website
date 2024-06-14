514df8938771a5999457306aa43e2d1c0fa570cf
Rylan <rylanbot@foxmail.com>
2024-06-14 15:25:24 +0800

    fix: title rendering error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
-import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import Layout from '@theme/Layout';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <div className={styles.hero} data-theme="dark">
       <div className={styles.heroInner}>
@@ -20,7 +18,9 @@ function HomepageHeader() {
               __html: 'The <b>Organization</b> for fzuers'
             }}
           />
-          <h2> 西二在线成立于 1998 年，是福州大学最大的学生组织</h2>
+        </Heading>
+        <Heading as="h2" className="text-white">
+          西二在线成立于 1998 年，是福州大学最大的学生组织
         </Heading>
         <div className={styles.indexCtas}>
           <Link className="button button--primary" to="/docs/intro">
@@ -36,7 +36,7 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
