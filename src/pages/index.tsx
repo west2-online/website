49161bc2f5d45f24aa705d6ab0b9b89258d106e2
吴荣榜 <1220045015@qq.com>
2024-09-04 18:41:59 +0800

    feat: 修改样式

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,35 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-import Lottie from 'react-lottie'
-import logoData from '../../static/lottie/logo.json'
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
+import Lottie from "react-lottie";
+import logoData from "../../static/lottie/logo.json";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
   return (
     <div className={styles.hero} data-theme="dark">
       <div className={styles.heroInner}>
         <Heading as="h1" className={styles.heroProjectTagline}>
-          <div className={clsx(styles.heroLogo, 'w-[200px]', 'aspect-square', 'bg-white')}>
+          <div
+            className={clsx(
+              styles.heroLogo,
+              "w-[200px]",
+              "aspect-square",
+              "bg-white"
+            )}
+          >
             <Lottie
               options={{
                 loop: false,
                 autoplay: true,
                 animationData: logoData,
                 rendererSettings: {
-                  preserveAspectRatio: 'xMidYMid slice'
-                }
+                  preserveAspectRatio: "xMidYMid slice",
+                },
               }}
               height={200}
               width={200}
@@ -32,18 +39,18 @@ function HomepageHeader() {
             className={styles.heroTitleTextHtml}
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
-              __html: 'The <b>Organization</b> for fzuers'
+              __html: "The <b>Organization</b> for FZUers",
             }}
           />
         </Heading>
-        <Heading as="h2" className='text-white'>
+        <Heading as="h2" className="text-white">
           西二在线成立于 1998 年，是福州大学最大的学生组织
         </Heading>
         <div className={styles.indexCtas}>
-          <Link className="button button--primary" to="/docs/intro">
+          <Link className="button button--primary text-black" to="/docs/intro">
             关于我们
           </Link>
-          <Link className="button button--info" to="/docs/recruitment">
+          <Link className="button button--secondary" to="/docs/recruitment">
             加入我们
           </Link>
         </div>
@@ -53,11 +60,12 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="west2-online's organization introduction index page">
+      description="west2-online's organization introduction index page"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
